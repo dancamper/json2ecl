39336242ef8ed2ea3fccd75ac79e22d925a3f742
Dan S. Camper <dan@bti.net>
2023-03-30 06:10:25 -0500

    Whitespace fixes
    Signed-off-by: Dan S. Camper <dan@bti.net>

--- build_binary.sh
+++ build_binary.sh
@@ -11,10 +11,10 @@ set -euo pipefail
 mkdir -p bin
 
 buildapp --output bin/json2ecl \
-         --eval "(let* ((home-dir (user-homedir-pathname)) (setup (or (probe-file (merge-pathnames \"quicklisp/setup.lisp\" home-dir)) (probe-file (merge-pathnames \"portacle/all/quicklisp/setup.lisp\" home-dir))))) (load setup))" \
-         --eval "(require :asdf)" \
-         --eval "(ql:quickload '(:adopt :com.inuoe.jzon :with-user-abort) :silent t)" \
-         --asdf-path . \
-         --load-system json2ecl \
-         --entry json2ecl:toplevel \
-         --compress-core
+  --eval "(let* ((home-dir (user-homedir-pathname)) (setup (or (probe-file (merge-pathnames \"quicklisp/setup.lisp\" home-dir)) (probe-file (merge-pathnames \"portacle/all/quicklisp/setup.lisp\" home-dir))))) (load setup))" \
+  --eval "(require :asdf)" \
+  --eval "(ql:quickload '(:adopt :com.inuoe.jzon :with-user-abort) :silent t)" \
+  --asdf-path . \
+  --load-system json2ecl \
+  --entry json2ecl:toplevel \
+  --compress-core
