e50dd5593c4ebf306b6577ac543bb76902edb776
Dan S. Camper <dan@bti.net>
2023-04-08 12:15:18 -0500

    Remove :silent so we can see progress better
    Signed-off-by: Dan S. Camper <dan@bti.net>

--- build_binary.sh
+++ build_binary.sh
@@ -13,7 +13,7 @@ mkdir -p bin
 buildapp --output bin/json2ecl \
   --eval "(let* ((home-dir (user-homedir-pathname)) (setup (or (probe-file (merge-pathnames \"quicklisp/setup.lisp\" home-dir)) (probe-file (merge-pathnames \"portacle/all/quicklisp/setup.lisp\" home-dir))))) (load setup))" \
   --eval "(require :asdf)" \
-  --eval "(ql:quickload '(:adopt :com.inuoe.jzon :with-user-abort) :silent t)" \
+  --eval "(ql:quickload '(:adopt :com.inuoe.jzon :with-user-abort))" \
   --asdf-path . \
   --load-system json2ecl \
   --entry json2ecl:toplevel \
